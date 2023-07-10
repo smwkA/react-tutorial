edeea36f630bfbe5f442b6772d4d909ef762b9a2
msuga <m-suga@stylagy.co.jp>
2023-07-10 16:45:32 +0900

    update(feat): helloスクリプトをHTML表示用に更新

--- src/hello.ts
+++ src/hello.ts
@@ -1,2 +1,18 @@
-const hello = (name: string): string => `Hello, ${name}!`;
-console.log(hello("World"));
+const hello = (name: string): void => {
+  const body = document.body
+  if (body) {
+    const hedding = document.createElement("h1")
+    hedding.textContent = `Hello ${name}`
+    body.appendChild(hedding)
+  }
+}
+
+const onLoad = (): void => {
+  hello("World")
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", onLoad)
+} else {
+  onLoad()
+}
