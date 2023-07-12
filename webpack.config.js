47c1604a5a43288109e5406f7f193884b2420423
suga_marin <m.suga.p@p1.techfirm.co.jp>
2023-07-12 16:55:02 +0900

    add(config): devServerのエラー修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 const path = require("path")
 
 module.exports = {
-    entry: "./src/App.tsx",
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "bundle.js",
+  entry: "./src/App.tsx",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "static"),
     },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-            },
-        ],
-    },
-    mode: "development",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "static"),
-        open: true,
-        port: 3000,
-    },
+    open: true,
+    port: 3000,
+  },
 }
